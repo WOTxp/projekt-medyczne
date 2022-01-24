b34766bb187474b0d7909a1f652d2eaf10694284
Acer <esp.matyjasekdomino@gmail.com>
2022-01-24 19:57:27 +0100

    tło, powrot do glownej i modyfikacja kategorii.html(nie skończone ale nie wpływa na działanie reszty)

--- medyczne/medyczne/Apteka/views.py
+++ medyczne/medyczne/Apteka/views.py
@@ -2,29 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Apteka, Kategoria
 
+
 # Create your views here.
 
-def index (request):
-    #wszystkie = Apteka.objects.all()
-    #jeden = Apteka.objects.get(pk=2)
+def index(request):
+    # wszystkie = Apteka.objects.all()
+    # jeden = Apteka.objects.get(pk=2)
     kat = Apteka.objects.filter(kategoria=1)
     null = Apteka.objects.filter(kategoria__isnull=False)
     kategorie = Kategoria.objects.all()
-    dane = {'kategorie' : kategorie}
+    dane = {'kategorie': kategorie}
     return render(request, 'szablon.html', dane)
 
-def kategoria (request, id):
+
+def kategoria(request, id):
     kategoria_user = Kategoria.objects.get(pk=id)
-    kategoria_apteka = Apteka.objects.filter(kategoria = kategoria_user)
+    kategoria_apteka = Apteka.objects.filter(kategoria=kategoria_user)
     kategorie = Kategoria.objects.all()
-    dane = {'kategoria_user' : kategoria_user,
-            'kategoria_produkt' : kategoria_apteka,
-            'kategoria' : kategorie}
+    dane = {'kategoria_user': kategoria_user,
+            'kategoria_produkt': kategoria_apteka,
+            'kategoria': kategorie}
     return render(request, 'kategorie_apteka.html', dane)
 
-def apteka (request, id):
+
+def apteka(request, id):
     apteka_user = Apteka.objects.get(pk=id)
     kategorie = Kategoria.objects.all()
-    dane = {'apteka_user' : apteka_user, 'kategorie' : kategorie}
+    dane = {'apteka_user': apteka_user, 'kategorie': kategorie}
     return render(request, 'apteka.html', dane)
-
